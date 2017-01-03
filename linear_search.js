d3814d1020d2e4b2f08070cdebedefc2ff5188d2
irwinpratajaya <irwinpratajaya@gmail.com>
2017-01-03 22:02:19 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,18 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var index = "";
+  var angka = [0,1,2,3,4,5,6,7,8];
+  for (var i = 0; i < values.length; i++) {
+  	if (target == values[i]) {
+  		index += angka[i];
+  		break;
+  	}
+  }
+  if (index == "") {
+  	return -1
+  }
+  return index
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -13,10 +25,20 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
+
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var index = []
+  // var angka = [0,1,2,3,4,5,6,7,8];
+
+  for (var i = 0; i < values.length; i++) {
+  	if (target == values[i]) {
+  		index.push(i)
+  	}
+  }
+  return index
 }
 
 let banana_arr = "banana".split("");
