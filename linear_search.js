bbdcef512e6e0a3e63528367a26441b47e166172
qblol <iqbal.0809@gmail.com>
2017-01-03 21:41:02 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var count = 0;
+  for (var i =0; i<values.length; i++) {
+    if (values.indexOf(target) == -1) {
+      count = -1
+    } else if(target == values[i]){
+      count+= 1
+    }
+  }
+  return count;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +26,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var tempArr = [];
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      tempArr.push(i)
+    }
+  }
+  return tempArr
 }
 
 let banana_arr = "banana".split("");
