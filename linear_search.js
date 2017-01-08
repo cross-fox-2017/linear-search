9ef3b698a927da1fe1a8c5a0e77e646c756a1971
Raditya <arya.raditya26@gmail.com>
2017-01-08 08:04:46 +0700

    selesai (release 0 & 1)

--- linear_search.js
+++ linear_search.js
@@ -1,25 +1,36 @@
-'use strict'
-
-// Release 0
-console.log("Linear Search")
-let linearSearch = (target, values) => {
-  //write your code here
+// release 0..
+function linearSearch(value,randomNum) {
+  for (var i = 0; i < randomNum.length; i++) {
+    if (randomNum[i] == value) {
+      return i
+    }
+  }
+  return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6,29,18,2,72,19,18,10,37]
 
 console.log(linearSearch(18, random_numbers));
 // 2
+console.log(linearSearch(10, random_numbers));
+// 7
 console.log(linearSearch(9, random_numbers));
 // -1
 
-// Release 1
-console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
-  //write your code here
+
+// release 1
+
+function globalLinearSearch(letter, word) {
+  let wordTemp = []
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      wordTemp.push(i)
+    }
+  }
+  return wordTemp
 }
 
-let banana_arr = "banana".split("");
-// ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
+let word_split = 'hakunamatata'.split('')
+
+console.log(globalLinearSearch('a', word_split));
+// [ 1, 5, 7, 9, 11 ]
